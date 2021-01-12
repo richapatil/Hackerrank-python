c5a6b578640fb120395ae23f09f8613769fa96a5
richapatil <54523621+richapatil@users.noreply.github.com>
2021-01-12 15:41:30 +0530

    Create Write_a_function.py

---
+++ Write_a_function.py
@@ -0,0 +1,17 @@
+#Solution
+
+def is_leap(year):
+    leap = False
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
