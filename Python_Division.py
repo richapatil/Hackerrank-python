ba0163784fa0a5f255dec0fb071d1d9ff71094bf
richapatil <54523621+richapatil@users.noreply.github.com>
2021-01-12 12:50:01 +0530

    Create Python_Division.py

---
+++ Python_Division.py
@@ -0,0 +1,15 @@
+#Solution 1:
+
+a = int(input())
+b = int(input())
+c = a//b
+d = a/b
+print (c)
+print (d)
+
+
+#Solution 2:
+
+a = int(input())
+b = int(input())
+print('{0} \n {1}'.format((a//b),(a/b)))
